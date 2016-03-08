971d5a21333ab09304690320158f144461d95fa7
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-03-08 02:09:38 +0100

    Initial commit with basic configuration

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+(function(){
+  'use strict';
+
+
+  module.exports = function (grunt) {
+
+  require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+      wiredep: {
+        task: {
+          // Point to the files that should be updated when
+          // you run `grunt wiredep`
+          src: [
+            'app/index.html'
+          ],
+          ignorePath:  /\.\.\//
+        }
+      },
+      sass: {
+        dist: {
+          files: {
+            'public/css/style.css': 'scss/style.scss'
+          }
+        }
+    },
+    jshint: {
+      options: {
+          reporter: require('jshint-stylish'),
+          jshintrc : '.jshintrc'
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          'app/js/{,*/}*.js'
+        ]
+      }
+    },
+      watch: {
+        project: {
+          files: ['public/**/*.js', 'public/**/*.html', 'public/**/*.json','public/**/*.css', './scss/*.scss'],
+          tasks: ["sass"],
+          options: {
+            livereload: true
+          },
+
+        },
+      },
+      connect: {
+        server: {
+          options: {
+            port: 9000,
+            base: 'public/',
+            open: true,
+            hostname: 'localhost',
+            livereload: 35729
+          }
+        }
+      }
+    });
+
+    grunt.registerTask('default', ['wiredep', 'connect', 'sass', 'jshint','watch']);
+
+  };
+})();
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# simple-project-template-angular
+# Simple Project Template for Angular
 A simple template for begin shortly a project with Angular, Sass and LiveReload preconfigured.
+
+### Version
+0.1.0
+
+This is project is a simple template for begin shortly a project with:
+
+* Angular
+* [SASS](http://sass-lang.com/guide)
+
+## Instalation
+
+* Download repository.
+* Install dependencies:
+
+	```sh
+	$ npm install
+	```
+	> Maybe you'll need to run npm install grunt-contrib-watch to get the last version of grunt-contrib-watch package. [More info](https://github.com/gruntjs/grunt-contrib-watch)
+
+* Run your project with:
+
+	```sh
+	$ grunt
+	```
+
+* Visit [localhost:9000](http://localhost:9000) to see your project
+
+## What do you get?
+
+Project structure:
+
+```
+.
+├── scss
+├── public
+│   ├── css
+│   ├── js
+│   └── index.html
+├── .gitignore
+├── .jshintrc
+├── Gruntfile.js
+├── package.json
+├── README.md
+
+```
+
+
+License
+-------
+
+[MIT](http://opensource.org/licenses/MIT)
