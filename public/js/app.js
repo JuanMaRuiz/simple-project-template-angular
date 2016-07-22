6be521766a41623fe27dac32492c0895eef3089b
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-07-22 18:07:58 +0200

    Added some styles, added menu and routes

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,24 @@
   'use strict';
   angular
     .module('bazingaApp', ['ui.router'])
-    .run(function() {
-        console.log("Bazinga!!!! angular app is running!!");
-      })
+    .config(configuration)
+
+    configuration.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function configuration($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'partials/state1.html'
+        })
+        .state('state1', {
+          url: '/state1',
+          templateUrl: 'partials/state1.html'
+        })
+        .state('state2', {
+          url: '/state2',
+          templateUrl: 'partials/state2.html'
+        });
+      $urlRouterProvider.otherwise('/');
+    }
 })();
