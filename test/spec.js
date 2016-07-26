a17948b4ebbc99441425cd0893b6dfa603509dc6
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-07-26 19:21:57 +0200

    Change Title on specs

--- test/spec.js
+++ test/spec.js
@@ -2,6 +2,6 @@ describe('Bazinga Demo App', function() {
   it('should have a title', function() {
     browser.get('http://localhost:9000/');
 
-    expect(browser.getTitle()).toEqual('Your Title');
+    expect(browser.getTitle()).toEqual('Bazinga Angular App');
   });
 });
