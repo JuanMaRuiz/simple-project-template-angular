2ea0a5d259606c5fe184afd87e396e67df9d3220
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-02-22 00:45:42 +0100

    Added basic karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'public/js/lib/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'public/js/**/*.js',
+      'public/js/lib/angular-ui-router.js',
+      'public/js/app.js',
+      'public/js/*.js',
       'public/**/*.html',
+      'public/**/*.css',
       'unit-tests/**/*.spec.js'
     ],
 
@@ -30,6 +34,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'public/js/*.js': 'coverage'
     },
 
 
