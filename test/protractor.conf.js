4a3194310f22bb0618484d3d087624f3f94ac7a0
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-02-21 21:50:29 +0100

    Added karma configuration

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,6 +1,14 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 exports.config = {
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
+  },
   specs: ['specs/*-Spec.js'],
   capabilities: {
     browserName: 'chrome'
