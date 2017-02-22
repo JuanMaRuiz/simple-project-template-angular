cb5d96037f160e3da12995de5bc57a65c126b8ce
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-02-22 21:22:14 +0100

    Removed npm-check-update packages and added npm outdated script to check dependencies

--- test/specs/state1-Spec.js
+++ test/specs/state1-Spec.js
@@ -1,9 +1,13 @@
-describe('Bazinga Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://localhost:9000/');
+describe('Bazinga Demo App, Home', function() {
 
+  beforeEach(function() {
+    browser.get('http://localhost:9000/');
+  });
+
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Bazinga Angular App');
   });
+
   it('should have and h1', function() {
     expect(element(by.tagName('h1')).getText()).toBe('Hooray! It\'s works!');
   });
